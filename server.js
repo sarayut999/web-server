024971fea7239449821cd7040b7d6521b076c9c4
Gus <pn.sarayut@gmail.com>
2019-01-28 20:13:02 +0700

    mockup todos

--- server.js
+++ server.js
@@ -2,12 +2,38 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id:1,
+	name: 'Gus',
+	completed: false
+},
+{
+	id:1,
+	name: 'Leng',
+	completed: true
+},
+{
+	id:1,
+	name: 'Ing',
+	completed: true
+},
+{
+	id:1,
+	name: 'Max',
+	completed: false
+}]
+
 var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('About us!');
+});
+
+// GET /todos
+app.get('/todos', middleware.requireAuthentication, function (req, res) {
+	res.json(todos);
 });
 
 app.use(express.static(__dirname + '/public'));
