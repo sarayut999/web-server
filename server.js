7d114d2051df47b49e4f54079489e732235497cd
Gus <pn.sarayut@gmail.com>
2019-02-04 14:02:31 +0700

    add post todos & npm underscore

--- server.js
+++ server.js
@@ -2,26 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id:1,
-	name: 'Gus',
-	completed: false
-},
-{
-	id:1,
-	name: 'Leng',
-	completed: true
-},
-{
-	id:1,
-	name: 'Ing',
-	completed: true
-},
-{
-	id:1,
-	name: 'Max',
-	completed: false
-}]
+var todos = [];
 
 var middleware = require('./middleware.js');
 
@@ -34,6 +15,40 @@ app.get('/about', middleware.requireAuthentication, function (req, res) {
 // GET /todos
 app.get('/todos', middleware.requireAuthentication, function (req, res) {
 	res.json(todos);
+});
+
+// GET /todo:id
+app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach( function (todo) {
+		if (todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo)
+	} else {
+		res.status(404).json({ errorMessage: 'is Not Data' });
+	}
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = _.pick(req.body, 'name', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.name) || body.name.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.name = body.name.trim();	
+	body.id = todoNextId++;
+
+	todos.push(body);
+	
+	res.json(body);
 });
 
 app.use(express.static(__dirname + '/public'));
